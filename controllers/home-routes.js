dab68419f4e077df1511a52787734ec01373e43d
Sherman Burwell <shermanburwell3@gmail.com>
2024-08-19 10:27:42 -0500

    Added homepage route and logic to home-routes.js and added api/index.js

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,2 +1,22 @@
-const router = require('express').Router();
-const { Gallery, Painting } = require('../models');
+const express = require('express');
+const { User, BlogPost } = require('../models');
+const router = express.Router();
+
+// GET route to display the home page
+router.get('/', async (req, res) => {
+  try {
+    // Fetch all blog posts with associated user information
+    const blogPosts = await BlogPost.findAll({
+      include: {
+        model: User,
+        attributes: ['username']
+      }
+    });
+
+    res.render('homepage', { blogPosts });
+  } catch (error) {
+    res.status(500).json({ error: 'Internal server error' });
+  }
+});
+
+module.exports = router;
