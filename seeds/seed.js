6db08f8a017b42d35894ebf5c18b334552474b11
Sherman Burwell <shermanburwell3@gmail.com>
2024-08-19 11:31:14 -0500

    Added blogpost seeds to seed.js

---
+++ seeds/seed.js
@@ -0,0 +1,23 @@
+const { User, BlogPost } = require('../models');
+
+const blogPostData = [
+  {
+    title: 'First Blog Post',
+    content: 'This is the content of the first blog post.',
+    user_id: 1
+  },
+  {
+    title: 'Second Blog Post',
+    content: 'This is the content of the second blog post.',
+    user_id: 2
+  },
+  {
+    title: 'Third Blog Post',
+    content: 'This is the content of the third blog post.',
+    user_id: 1
+  }
+];
+
+const seedBlogPosts = () => BlogPost.bulkCreate(blogPostData);
+
+module.exports = seedBlogPosts;
